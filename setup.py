f3139c42ee0d4cc9a54675f20501a89c1cf6690e
Damir Jelić <poljar@termina.org.uk>
2019-03-25 14:11:51 +0100

    setup.py: Remove unneeded dependency.

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ setup(
         "attrs",
         "aiohttp",
         "appdirs",
-        "aiohttp-socks",
         "typing;python_version<'3.5'",
         "matrix-nio @ git+https://github.com/poljar/matrix-nio.git@async#egg=matrix-nio-0"
     ],
