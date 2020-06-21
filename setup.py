da08ccac5a8eac92a1fa8c7572fb06c949de96d5
Damir Jelić <poljar@termina.org.uk>
2020-06-21 21:40:29 +0200

    setup.py: Bump the nio version.

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         "cachetools >= 3.0.0"
         "prompt_toolkit>2<4",
         "typing;python_version<'3.5'",
-        "matrix-nio[e2e] <= 0.13"
+        "matrix-nio[e2e] <= 0.14"
     ],
     extras_require={
         "ui": [
